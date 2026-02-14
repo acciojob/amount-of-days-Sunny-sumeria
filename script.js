694457cf463332a1b21c2cd37ac44e8bc6344b02
Sunny Sumeria <130832961+Sunny-sumeria@users.noreply.github.com>
2026-02-14 09:11:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+function daysOfAYear(year) {
+  if ((year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0)) {
+    return 366;
+  } else {
+    return 365;
+  }
+}
+const testYears = [
+  2022, 
+  2024, 
+  1900,
+  2000
+];
+testYears.forEach(year => {
+  const days = daysOfAYear(year);
+  console.log(`Year: ${year} -> Days: ${days}`);
+});
+
